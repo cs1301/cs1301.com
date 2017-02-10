3cfe3e17a8871fddac6e1d7ba549c07fc7488275
Joshua Diaddigo <joshuad95@hotmail.com>
2017-02-10 11:54:31 -0500

    changing lab2 to lab1

--- downloads/lab2/style.php
+++
@@ -1,6 +0,0 @@
-<?php
-header('Content-Type: application/html');
-header("Content-disposition: attachment; filename=\"style.css\"");
-$hidden_char = json_decode('"\u200c"');
-$index = file_get_contents("style.css");
-echo str_replace("UNIQUE_ID", str_repeat($hidden_char,rand(0, 1000))." ".str_repeat($hidden_char,rand(0, 1000))." ".str_repeat($hidden_char,rand(0, 1000))." ".str_repeat($hidden_char,rand(0, 1000)), $index);
