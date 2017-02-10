e738dd8a84a8207496971a394e6098707b6c394e
Joshua Diaddigo <joshuad95@hotmail.com>
2017-02-10 12:51:03 -0500

    adding google analytics

--- resources/js/main.js
+++ resources/js/main.js
@@ -8,6 +8,13 @@ jsh.addEventListener("ready", function() {
     bind_listeners();
 
     if (window.location.href.indexOf("#") == -1) jsh.pages["syllabus"].open();
+
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-50036831-3', 'auto');
+    ga('send', 'pageview');
 });
 
 function build_dom() {
